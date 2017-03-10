ab3a96ce7ee7fb204dcab90ca83e83e5dc1d5d69
baconLIN001 <2803873907@qq.com>
2017-03-10 18:03:19 +0800

    Initial commit

---
+++ src/HeapOOM.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by bacon on 2017/3/10.
+ * VM Args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ */
+public class HeapOOM {
+    static class OOMObject{
+
+    }
+
+    public static void main(String[] args){
+        List<OOMObject> list = new ArrayList<OOMObject>();
+
+        while (true)
+        {
+            list.add(new OOMObject());
+        }
+    }
+}
