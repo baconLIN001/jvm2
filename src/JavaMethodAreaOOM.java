ad756576a45b994d820edaca64795c5e292d494b
baconLIN <baconLIN@outlook.com>
2017-03-12 01:13:31 +0800

    jvm第二章内存溢出案例

---
+++ src/JavaMethodAreaOOM.java
@@ -0,0 +1,29 @@
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * VM Args: -XX:PermSize=10M -XX:MazPermSIze=10M
+ * Created by baconLIN on 2017/3/12.
+ */
+public class JavaMethodAreaOOM {
+    public static void main(String[] args){
+        while (true){
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(OOMObject.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                @Override
+                public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                    return methodProxy.invokeSuper(o,objects);
+                }
+            });
+            enhancer.create();
+        }
+    }
+    static class OOMObject{
+
+    }
+}
